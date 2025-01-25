4a5a7af44de67754d107380cae8abf1ef7aa114d
MOHAMMED ASLAM <aslam.mohd14338@gmail.com>
2025-01-25 15:05:17 -0500

    3.05 PM, Login Completed. Alhamdulillah

--- ratingsapp/lib/login.dart
+++ ratingsapp/lib/login.dart
@@ -1,5 +1,59 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  User? _user;
+  @override
+  void initState() {
+    super.initState();
+    _auth.authStateChanges().listen((event){
+      setState(() {
+        _user = event;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(
+        title: const Text('Login Page'),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0), // Customize as needed
+      ),
+      body: _user != null ? _userInfo() : _googleSignInButton(),
+    );
+  }
+  Widget _googleSignInButton(){
+    return Center(child: SizedBox(
+      height: 50, 
+      child: SignInButton(Buttons.Google, text: "Sign up with Google", onPressed: _handleGoogleSignIn/*(){}*/),));
+  }
+  Widget _userInfo(){
+    return SizedBox();
+  }
+  void _handleGoogleSignIn(){
+    try{
+      GoogleAuthProvider googleAuthProvider = GoogleAuthProvider();
+      _auth.signInWithProvider(googleAuthProvider);
+    }
+    catch(error){
+      // ignore: avoid_print
+      print(error);
+    }
+  }
+}
+
+/*import 'package:flutter/material.dart';
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -36,7 +90,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
--- ratingsapp/lib/main.dart
+++ ratingsapp/lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ratingsapp/firebase_options.dart';
 import 'package:ratingsapp/home.dart';
 import 'package:ratingsapp/login.dart';
 import 'package:ratingsapp/info.dart';
 import 'package:ratingsapp/share.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
