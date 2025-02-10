81163a13d947b09d9c636250734be5b9da2599af
MOHAMMED ASLAM <aslam.mohd14338@gmail.com>
2025-02-10 14:12:12 -0500

    Alhamdulillah, Subhan Allah, Allah hu akbar, Bismillah, Subhan Allah, Completed database like/dislike functionality with the permission of Allah, Alhamdulillah

--- ratingsapp/lib/home.dart
+++ ratingsapp/lib/home.dart
@@ -26,19 +26,11 @@ class _HomePageState extends State<HomePage> {
       final querySnapshot = await db.collection("users").get();
       List<Map<String, dynamic>> fetchedItems = [];
       for (var doc in querySnapshot.docs) {
-        if (doc.data()['items'] != null) {
-          List<dynamic> userItems = doc.data()['items'];
-          for (var item in userItems) {
-            fetchedItems.add({
-              'id': item['id'], // Ensure each item has a unique identifier
-              'professorName': item['professorName'],
-              'classCode': item['classCode'],
-              'universityCode': item['universityCode'],
-              'yesCount': item['yesCount'] ?? 0,
-              'noCount': item['noCount'] ?? 0,
-            });
-          }
-        }
+        var userItems = List<Map<String, dynamic>>.from(doc.data()['items'] ?? []);
+        userItems.forEach((item) {
+          item['docId'] = doc.id; // Store document ID for reference
+          fetchedItems.add(item);
+        });
       }
       setState(() {
         allItems = fetchedItems;
@@ -52,50 +44,28 @@ class _HomePageState extends State<HomePage> {
   void _filterItems(String query) {
     setState(() {
       filteredItems = allItems
-          .where((item) => item['professorName']
-              .toString()
-              .toLowerCase()
-              .contains(query.toLowerCase()))
+          .where((item) =>
+              item['professorName'].toString().toLowerCase().contains(query.toLowerCase()))
           .toList();
     });
   }
 
-  void _handleYes(int index) async {
+  Future<void> _handleVote(int index, bool isYesVote) async {
     final item = filteredItems[index];
-    final newYesCount = item['yesCount'] + 1;
-
-    // Update the local state
-    setState(() {
-      filteredItems[index]['yesCount'] = newYesCount;
-    });
-
-    // Update Firestore
-    try {
-      await db.collection("users").doc(item['id']).update({
-        'yesCount': newYesCount,
-      });
-    } catch (e) {
-      print('Error updating yesCount: $e');
-    }
-  }
-
-  void _handleNo(int index) async {
-    final item = filteredItems[index];
-    final newNoCount = item['noCount'] + 1;
-
-    // Update the local state
-    setState(() {
-      filteredItems[index]['noCount'] = newNoCount;
-    });
-
-    // Update Firestore
-    try {
-      await db.collection("users").doc(item['id']).update({
-        'noCount': newNoCount,
-      });
-    } catch (e) {
-      // ignore: avoid_print
-      print('Error updating noCount: $e');
+    final docId = item['docId']; // Retrieve the document ID
+    final userDoc = await db.collection("users").doc(docId).get();
+    if (userDoc.exists) {
+      List<dynamic> items = List<dynamic>.from(userDoc.data()?['items'] ?? []);
+      int itemIndex = items.indexWhere((i) =>
+          i['professorName'] == item['professorName'] &&
+          i['classCode'] == item['classCode']);
+      if (itemIndex != -1) {
+        items[itemIndex][isYesVote ? 'yesCount' : 'noCount']++;
+        await db.collection("users").doc(docId).update({'items': items});
+        setState(() {
+          filteredItems[index][isYesVote ? 'yesCount' : 'noCount'] = items[itemIndex][isYesVote ? 'yesCount' : 'noCount'];
+        });
+      }
     }
   }
 
@@ -110,15 +80,12 @@ class _HomePageState extends State<HomePage> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              style: const TextStyle(color: Color.fromARGB(255, 255, 17, 0)),
               controller: searchController,
               onChanged: _filterItems,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Search items',
-                labelStyle: TextStyle(
-                  color: Color.fromARGB(255, 55, 0, 255),
-                ),
+                labelStyle: TextStyle(color: Color.fromARGB(255, 55, 0, 255)),
               ),
             ),
           ),
@@ -134,93 +101,90 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   )
-                : Container(
-                    color: Colors.black12,
-                    child: ListView.builder(
-                      itemCount: filteredItems.length,
-                      itemBuilder: (context, index) {
-                        final item = filteredItems[index];
-                        return Card(
-                          color: const Color.fromARGB(103, 76, 75, 75),
-                          child: ListTile(
-                            title: Text(
-                              '${item['professorName']}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.blue,
-                              ),
+                : ListView.builder(
+                    itemCount: filteredItems.length,
+                    itemBuilder: (context, index) {
+                      final item = filteredItems[index];
+                      return Card(
+                        color: const Color.fromARGB(103, 76, 75, 75),
+                        child: ListTile(
+                          title: Text(
+                            item['professorName'],
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
                             ),
-                            subtitle: Text(
-                              'Course: ${item['classCode']}\nUniversity: ${item['universityCode']}',
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 0, 68, 255),
-                              ),
-                            ),
-                            trailing: SingleChildScrollView(
-                              child: Container(
-                                padding: const EdgeInsets.all(8.0), // Adds padding inside the container
-                                decoration: BoxDecoration(
-                                  color: const Color.fromARGB(255, 0, 0, 0), // Background color
-                                  borderRadius: BorderRadius.circular(8.0), // Rounded corners
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.black26, // Soft shadow effect
-                                      blurRadius: 4.0,
-                                      offset: Offset(0, 2),
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        IconButton(
-                                          icon: const Icon(Icons.check, color: Color.fromARGB(255, 0, 255, 8)),
-                                          onPressed: () => _handleYes(index),
-                                        ),
-                                        IconButton(
-                                          icon: const Icon(Icons.close, color: Color.fromARGB(255, 255, 0, 0)),
-                                          onPressed: () => _handleNo(index),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Text(
-                                          '${item['yesCount']}',
-                                          style: const TextStyle(
-                                            fontSize: 12, // Increased font size
-                                            fontWeight: FontWeight.bold, // Bold text
-                                            color: Color.fromARGB(255, 89, 255, 0), // Text color
-                                          ),
-                                        ),
-                                        const SizedBox(width: 40),
-                                        Text(
-                                          '${item['noCount']}',
-                                          style: const TextStyle(
-                                            fontSize: 12, // Increased font size
-                                            fontWeight: FontWeight.bold, // Bold text
-                                            color: Color.fromARGB(255, 255, 0, 0), // Text color
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-
-
                           ),
-                        );
-                      },
-                    ),
+                          subtitle: Text(
+                            'Course: ${item['classCode']}\nUniversity: ${item['universityCode']}',
+                            style: const TextStyle(color: Color.fromARGB(255, 0, 68, 255)),
+                          ),
+                          trailing: _buildTrailingColumn(item, index),
+                        ),
+                      );
+                    },
                   ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildTrailingColumn(Map<String, dynamic> item, int index) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 0, 0, 0),
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 4.0,
+              offset: Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.check, color: Color.fromARGB(255, 0, 255, 8)),
+                  onPressed: () => _handleVote(index, true),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close, color: Color.fromARGB(255, 255, 0, 0)),
+                  onPressed: () => _handleVote(index, false),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '${item['yesCount']}',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 89, 255, 0),
+                  ),
+                ),
+                const SizedBox(width: 40),
+                Text(
+                  '${item['noCount']}',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 0, 0),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
